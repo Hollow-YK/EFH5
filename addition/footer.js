56e2b3a732601d5050b96aa5bfa4a8e734bb11bc
Hollow <116991734+Hollow-YK@users.noreply.github.com>
2024-12-18 15:04:10 +0800

    Add files via upload

---
+++ addition/footer.js
@@ -0,0 +1,12 @@
+function text_print (str){
+	document.write (str);
+}
+function footer_notice() {
+	text_print("公告");
+}
+function footer_links() {
+	text_print("<a href='友情链接URL'>友情链接标题</a><br>");
+}
+function copyright() {
+	text_print("版权声明")
+}
